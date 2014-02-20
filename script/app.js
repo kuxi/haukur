5cb8b725f96902a3a84306c6802bc391bafa28db
haukurjarl <haukur@nordicemarketing.com>
2014-02-20 22:42:23 +0000

    JavaScript, CSS update
    Login function complete
    More Babes included

--- script/app.js
+++ script/app.js
@@ -3,23 +3,35 @@ require([], function() {
     var submitButton = document.getElementById('submit');
     var username_field = document.getElementById('username');
     var password_field = document.getElementById('password');
+    var container = document.getElementById('container');
+    var goodbyelove = document.getElementById("closeButton");
 
     // Create a list of user objects
     // Each user contains a username and password length
     var all_users = [
         { username: 'haukur', password_length: 4 },
-        { username: 'heidar', password_length: 5 }
+        { username: 'heidar', password_length: 5 },
+        { username: 'dilja', password_length: 9 },
+        { username: 'klara', password_length: 7 }
     ];
 
     function is_valid(username, password) {
-        // If the password is of the correct length for a given username
-        // then we return true.
-        // If the username does not exist in all_users, then return false
-        // If the username exists but the password length is incorrect,
-        // then we return false
+        for (var i = 0; i < all_users.length; i++){
+		    var user = all_users[i]
+    		if (user["username"] === username){
+	    		if (password.length === user["password_length"]){
+		    		return true;
+		    	}
+
+    		}   
+			
+	    };
         return false;
     }
 
+    function show_babe() {
+        container["style"]["display"] = "block";
+    }
     // Attach a function to the submit button's onclick event
     // The function will be run every time the button is pressed
     submitButton.onclick = function(event) {
@@ -32,9 +44,21 @@ require([], function() {
 
         // Call our helper function and act according to its result
         if(is_valid(username, password)) {
-            alert('Hello, ' + username + '!');
+            show_babe();
         } else {
             alert('Invalid username/password');
         }
     };
+    goodbyelove.onclick = function(event) {
+        container["style"]["display"] = "";
+    }
 });
+
+
+
+
+
+
+
+
+
